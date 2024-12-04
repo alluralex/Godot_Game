7cba3c825b178d3d97d86b0af4ccac0130c39e9d
alluralex <alluralexk@gmail.com>
2024-12-05 00:48:06 +1000

    Oh I crush my PC
    3FPS

--- CardDungeon/scenes/Menu/HeroChoose.cs
+++ CardDungeon/scenes/Menu/HeroChoose.cs
@@ -3,35 +3,93 @@ using System;
 
 public partial class HeroChoose : Node2D
 {
+	public HeroChoose() 
+	{
+        
+    }
+
 	public override void _Ready()
 	{
+		
 	}
 
-	public override void _Process(double delta)
-	{
-	}
-	private void _on_control_warrior_mouse_entered()
-	{
-		GetNode<AnimatedSprite2D>("Control_Warrior/Circle_Warrior/w_sprites").Animation = "walk";
-	}
-	private void _on_control_warrior_mouse_exited()
-	{
-		GetNode<AnimatedSprite2D>("Control_Warrior/Circle_Warrior/w_sprites").Animation = "idle";
-	}
-	private void _on_control_mag_mouse_entered()
-	{
-		GetNode<AnimatedSprite2D>("Control_Mag/Circle_Mag/m_sprites").Animation = "walk";
-	}
-	private void _on_control_mag_mouse_exited()
-	{
-        GetNode<AnimatedSprite2D>("Control_Mag/Circle_Mag/m_sprites").Animation = "idle";
+    private async void _on_button_warrior_pressed()
+    {
+        var stateMage = GetNode<Sprite2D>		("States/StateHeroMage");
+        var stateWarrior = GetNode<Sprite2D>	("States/StateHeroWarrior");
+        var stateArcher = GetNode<Sprite2D>		("States/StateHeroArcher");
+        var empty = GetNode<Sprite2D>			("States/Empty");
+        var startRun_B = GetNode<Button>		("StartRun");
+
+        stateWarrior.Visible = true;
+        startRun_B.Visible = true;
+
+        stateMage.Visible = false;
+        stateArcher.Visible = false;
+        empty.Visible = false;
     }
-	private void _on_control_archer_mouse_entered()
-	{
-        GetNode<AnimatedSprite2D>("Control_Archer/Circle_Archer/a_sprites").Animation = "walk";
+
+    private async void _on_button_archer_pressed()
+    {
+        var stateMage = GetNode<Sprite2D>		("States/StateHeroMage");
+        var stateWarrior = GetNode<Sprite2D>	("States/StateHeroWarrior");
+        var stateArcher = GetNode<Sprite2D>		("States/StateHeroArcher");
+        var empty = GetNode<Sprite2D>			("States/Empty");
+        var startRun_B = GetNode<Button>		("StartRun");
+
+        stateArcher.Visible = true;
+        startRun_B.Visible = true;
+
+        stateWarrior.Visible = false;
+        stateMage.Visible = false;
+        empty.Visible = false;
     }
-	private void _on_control_archer_mouse_exited()
-	{
-        GetNode<AnimatedSprite2D>("Control_Archer/Circle_Archer/a_sprites").Animation = "idle";
+
+    private async void _on_button_mage_pressed()
+    {
+        var stateMage = GetNode<Sprite2D>		("States/StateHeroMage");
+        var stateWarrior = GetNode<Sprite2D>	("States/StateHeroWarrior");
+        var stateArcher = GetNode<Sprite2D>		("States/StateHeroArcher");
+        var empty = GetNode<Sprite2D>			("States/Empty");
+        var startRun_B = GetNode<Button>		("StartRun");
+
+		stateMage.Visible = true;
+		startRun_B.Visible = true;
+
+		stateWarrior.Visible = false;
+		stateArcher.Visible = false;
+		empty.Visible = false;
+
+
     }
+
+    public override void _Process(double delta)
+	{
+	}
+	private void _on_button_warrior_mouse_entered()
+	{
+		GetNode<AnimatedSprite2D>("Heroes/Button_Warrior/Circle_Warrior/w_sprites").Animation = "walk";
+	}
+	private void _on_button_warrior_mouse_exited()
+	{
+		GetNode<AnimatedSprite2D>("Heroes/Button_Warrior/Circle_Warrior/w_sprites").Animation = "idle";
+	}
+	private void _on_button_mage_mouse_entered()
+	{
+		GetNode<AnimatedSprite2D>("Heroes/Button_Mage/Circle_Mage/m_sprites").Animation = "walk";
+	}
+	private void _on_button_mage_mouse_exited()
+	{
+		GetNode<AnimatedSprite2D>("Heroes/Button_Mage/Circle_Mage/m_sprites").Animation = "idle";
+	}
+	private void _on_button_archer_mouse_entered()
+	{
+		GetNode<AnimatedSprite2D>("Heroes/Button_Archer/Circle_Archer/a_sprites").Animation = "walk";
+	}
+	private void _on_button_archer_mouse_exited()
+	{
+		GetNode<AnimatedSprite2D>("Heroes/Button_Archer/Circle_Archer/a_sprites").Animation = "idle";
+	}
+	
+	
 }
